dff2448f1679c88259cc6ae5fb5bad3746395ece
Joe Petroske <jmpetroske@gmail.com>
2015-10-18 08:04:18 -0700

    Added functionality to website file so it sends a json object via post

--- website/request_handler.js
+++ website/request_handler.js
@@ -1,16 +1,30 @@
+/*
+{
+	hashtag: "Shit", 
+	bad_words: ['weed', 'drugs', 'heroin', 'meth', 'porn']
+}
+*/
+
+
 window.onload = function (){
 	document.getElementById("submit_button").onclick = get_images;
 }
 
 function get_images (){
 	var show_blocked = document.getElementById("show_blocked_radio").checked;
-	var hashtag = document.getElementById("hashtag").value;
-	var blocked_words = document.getElementById("blocked_words_textbox").value;
+	var input_hashtag = document.getElementById("hashtag").value;
+	var blocked_text = document.getElementById("blocked_words_textbox").value;
+
+	var blocked_words = blocked_text.split(',');
+	for (var i = 0; i < blocked_words.length; i++){
+		blocked_words[i] = blocked_words[i].trim();
+	}
+	
+	var info = {hashtag:input_hashtag, bad_words:blocked_words};
+	console.log(info);
 
 	var params = new FormData();
-	params.append("hashtag", hashtag);
-	params.append("blocked_words", blocked_words);
-	params.append("show_blocked", show_blocked);
+	params.append("info", info);
 
 	var request = new XMLHttpRequest();
 	request.onload = processImages;
